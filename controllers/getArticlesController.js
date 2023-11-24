896f25658adea90c600d488f7cb0c411535463da
holyseo <128103221+holyseo@users.noreply.github.com>
2023-11-24 11:00:24 +0000

    task 11

--- controllers/getArticlesController.js
+++ controllers/getArticlesController.js
@@ -1,7 +1,17 @@
 const { selectAllArticles } = require("../models/articles");
+const { selectArticleByTopic } = require("../models/comments");
 
-exports.getAllArticles = (req, res) => {
-  selectAllArticles().then((articles) => {
-    res.status(200).send({ articles });
-  });
+exports.getAllArticles = (req, res, next) => {
+  const { topic } = req.query;
+  if (req.originalUrl === "/api/articles" || topic.length === 0) {
+    selectAllArticles().then((articles) => {
+      res.status(200).send({ articles });
+    });
+  }
+
+  selectArticleByTopic(topic)
+    .then((articles) => {
+      res.status(200).send({ articles });
+    })
+    .catch(next);
 };
