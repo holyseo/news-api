6c9c3ba887b8ff00a7c71d9415b79fbd7b5557be
holyseo <128103221+holyseo@users.noreply.github.com>
2023-12-11 10:08:18 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const { getTopics } = require("./controllers/getTopicsController");
 const { getEndpoints } = require("./controllers/getEndpointsController");
 const { getArticlesById } = require("./controllers/getArticlesByIdController");
@@ -20,7 +19,6 @@ const { getAllUsers } = require("./controllers/getAllUsersController");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/api", getEndpoints);
