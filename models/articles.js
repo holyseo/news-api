321e26f45308914c72f9428b49a847afe53cce9e
holyseo <128103221+holyseo@users.noreply.github.com>
2023-12-11 14:01:54 +0000

    fix 2

--- models/articles.js
+++ models/articles.js
@@ -60,21 +60,9 @@ exports.selectAllArticles = () => {
       articles.created_at,
       articles.votes,
       articles.article_img_url,
-      COUNT(comments.article_id) AS comment_count
-   FROM
-      articles
-   LEFT JOIN
-      comments ON articles.article_id = comments.article_id
-   GROUP BY
-      articles.author,
-      articles.title,
-      articles.article_id,
-      articles.topic,
-      articles.created_at,
-      articles.votes,
-      articles.article_img_url
-   ORDER BY
-      articles.created_at DESC;
+      (SELECT COUNT(*)::Int FROM comments WHERE comments.article_id = articles.article_id) AS comment_count 
+      FROM articles 
+      ORDER BY articles.created_at DESC;
     `
     )
     .then(({ rows }) => {
